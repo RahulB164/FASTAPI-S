aaaf82d2632ce1a8281896b72e477c36bd07363b
Rahul <60459782+RahulB164@users.noreply.github.com>
2025-08-26 18:29:00 +0530

    Tkinter Import

---
+++ hello.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+my_label = Label(root, text="Sneha , my World!") 
+
+my_label.pack() #Shoving it on to the screen
+
+root.mainloop() #Keeps the window open
+
